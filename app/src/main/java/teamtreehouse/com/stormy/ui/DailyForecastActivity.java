32de5b7614366187706319f946e3af513dde0ccb
Tianyang Zhong <tianyangzhong2013@gmail.com>
2017-08-08 12:28:02 -0400

    adding new list activity

---
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
@@ -0,0 +1,18 @@
+package teamtreehouse.com.stormy.ui;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import teamtreehouse.com.stormy.R;
+
+public class DailyForecastActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+
+    }
+
+}
